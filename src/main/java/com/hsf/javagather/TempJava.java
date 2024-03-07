f9a963f1ea996de0da10a905806c947d617bd021
ALittleDaisy <1152594956@qq.com>
2024-03-07 10:58:31 +0800

    AtomicIntegerArray和AtomicIntegerFieldUpdater

--- src/main/java/com/hsf/javagather/TempJava.java
+++ src/main/java/com/hsf/javagather/TempJava.java
@@ -9,12 +9,21 @@ public class TempJava {
     public static final String channel = "orgnic";
     public static final int personId = 123;
 
+    public enum BizMode {
+        h5,
+        web2app;
+    }
+
     public static void main(String[] args) {
         String channelValue = ReflectUtil.getStaticField(TempJava.class, "channel");
         System.out.println("看看渠道: " + channelValue);
 
         int personId = ReflectUtil.getStaticField(TempJava.class, "personId");
         System.out.println("个人Id: " + personId);
+
+        BizMode bbb = BizMode.h5;
+//        String sss = (String) BizMode.h5;
+        System.out.println(String.valueOf(BizMode.web2app));
     }
 
 
