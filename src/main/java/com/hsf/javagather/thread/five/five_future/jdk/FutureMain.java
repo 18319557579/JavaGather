603e329e17c9af2539e64a5207dfc61547e0af9d
ALittleDaisy <1152594956@qq.com>
2024-03-27 15:27:57 +0800

    JDK中的Future使用

---
+++ src/main/java/com/hsf/javagather/thread/five/five_future/jdk/FutureMain.java
@@ -0,0 +1,26 @@
+package com.hsf.javagather.thread.five.five_future.jdk;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+public class FutureMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> future = new FutureTask<>(new RealData("a"));
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(future);
+
+        System.out.println("请求完毕");
+
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+
+        }
+
+        System.out.println("数据 = " + future.get());
+
+        executorService.shutdown();
+    }
+}
